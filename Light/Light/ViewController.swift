db3435da92ea2e9f3fc13cf9b9efb23d19a9f6c2
liana <hilllia@ssfs.org>
2020-02-03 13:38:17 -0500

    worked on Light App

---
+++ Light/Light/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Liana on 1/31/20.
+//  Copyright © 2020 Liana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var lightOn = true
+    
+    @IBOutlet weak var lightButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .cyan
+        } else {
+            view.backgroundColor = .blue
+            }
+    }
+}
+
