5973de73d4b305c17cd6abfe30b45c10fd91b40f
Liana Hill <Liana@Cathalene-MacBook-Air.local>
2020-03-20 10:30:59 -0400

    finished Structures Playground

--- Lab - Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
@@ -3,18 +3,31 @@
  
  If you completed the exercise Structs, Instances, and Default Values, you created a `GPS` struct with default values for properties of `latitude` and `longitude`. Create your `GPS` struct again, but this time do not provide default values. Both properties should be of type `Double`.
  */
+struct GPS {
+    var latitude = Double()
+    var longitude = Double()
+}
 
-
-/*:
- Now create a constant instance of `GPS` called `somePlace`, and use the memberwise initializer to set `latitude` to 51.514004, and `longitude` to 0.125226. Print the values of `somePlace`'s properties.
- */
 
 
 /*:
  In Structs, Instance, and Default Values, you also created a `Book` struct. Creat this struct again without default values. Give each property appropriate types. Declare your `favoriteBook` instance and pass in the values of your favorite book using the memberwise initializer. Print a statement about your favorite book using `favoriteBook`'s properties.
  */
+struct Book {
+    var title = String()
+    var author = String()
+    var pages = Double()
+    var price = Double()
+}
 
+var favoriteBook = Book()
+print(favoriteBook)
+favoriteBook.title = "We Were Liars"
+favoriteBook.author = "E. Lockhart"
+favoriteBook.pages = 227
+favoriteBook.price = 12.93
 
+print("My favorite book is \(favoriteBook.title). It is written by \(favoriteBook.author). It is \(favoriteBook.pages) long and costs \(favoriteBook.price) dollars.")
 /*:
  Make a `Height` struct with two variable properties, `heightInInches` and `heightInCentimeters`. Both should be of type `Double`.
  
@@ -22,16 +35,30 @@
  
  - Example: If you use the initializer for inches to pass in a height of 65, the initializer should set `heightInInches` to 65 and `heightInCentimeters` to 165.1.
  */
+struct Height {
+    var heightInInches = Double()
+    var heightInCentimeters = Double()
 
+    init(heightInInches: Double) {
+        self.heightInCentimeters = heightInInches * 2.54
+        self.heightInInches = heightInInches
+    }
+    init(heightInCentimeters: Double) {
+        self.heightInInches = heightInCentimeters / 2.54
+        self.heightInCentimeters = heightInCentimeters
+    }
+}
 
 /*:
  Now create a variable instance of `Height` called `someonesHeight`. Use the initializer for inches to set the height to 65. Print out the property for height in centimeters and verify that it is equal to 165.1.
  */
-
+var someonesHeight = Height(heightInInches: 65)
+print(someonesHeight.heightInCentimeters)
 
 /*:
  Now create a variable instance of `Height` called `myHeight` and initialize it with your own height. Verify that both `heightInInches` and `heightInCentimeters` are accurate.
  */
-
+var myHeight = Height(heightInInches: 68)
+print(myHeight.heightInCentimeters)
 
 //: [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Users and Distance](@next)
--- Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -8,7 +8,14 @@ struct Book {
     var author: String
     var pages: Int
     var price: Double
+    
+    func description() {
+        print("The author of \(title) is \(author). The book has \(pages) and costs \(price) dollars.")
+    }
 }
+
+let newBook = Book(title: "The Hunger Games", author: "Susan Collins", pages: 400, price: 15)
+newBook.description()
 /*:
  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
  */
@@ -16,5 +23,13 @@ struct Post {
     var message: String
     var likes: Int
     var numberOfComments: Int
+    mutating func like() {
+        likes += 1
+    }
 }
+
+var photo = Post(message: "Be kind", likes: 14278574, numberOfComments: 957)
+print(photo.likes)
+photo.like()
+print(photo.likes)
 //: [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Workout Functions](@next)
