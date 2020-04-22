8f8f4491a78674d375f5e163d88008a8445234e4
Liana Hill <Liana@Cathalene-MacBook-Air.local>
2020-04-22 15:21:44 -0400

    worked on creating table view controllers and view controllers for the different exercises in workouts feature, need to figure out how to get screens for each exercise

---
+++ Final App Project/Final App Project/Exercise.swift
@@ -0,0 +1,15 @@
+//
+//  Exercise.swift
+//  Final App Project
+//
+//  Created by Liana Hill on 4/22/20.
+//  Copyright © 2020 Liana Hill. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Exercise {
+    var description: String
+    var image: UIImage
+}
