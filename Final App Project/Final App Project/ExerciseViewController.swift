3ed7b187389618bd48c5cb73858d44212c80f84e
Liana Hill <Liana@Cathalene-MacBook-Air.local>
2020-04-27 10:28:05 -0400

    worked on goals, researched images and exercise websites, inserted images into project, created a view controller for the exercises, but for some reason I can't drag image view into the exercise view controller to make a property

---
+++ Final App Project/Final App Project/ExerciseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExerciseViewController.swift
+//  Final App Project
+//
+//  Created by Liana Hill on 4/27/20.
+//  Copyright © 2020 Liana Hill. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
